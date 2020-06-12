f9f8f344e84b82d0da1a933cdc389bc396276df8
Justin Tam <tamtzekin@gmail.com>
2020-06-12 13:03:31 +1000

    Multiple buttons trigger fade in/out

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,53 @@
 // Initialise hint buttons
-const btn = document.querySelector('.js-btn');
-const el = document.querySelector('.js-fade');
+// const btn = document.querySelector('.js-btn');
 
-btn.addEventListener('click', function(e){
-  if(el.classList.contains('is-hidden')){
-	fadeIn(el);
-	console.log("Button clicked - fade in");
-  }
-  else {
-	fadeOut(el);
-	console.log("Button clicked - fade out");
-  }
-});
+const btn = document.querySelectorAll('span[class^=button]');
+console.log("Found", btn.length, "span class which starts with “button”.");
 
-// Hint button fade animations 
-function fadeOut(el) {
-	el.style.opacity = 1;
-	(function fade() {
-		if ((el.style.opacity -= .1) < 0) {
-			el.style.display = 'none';
-			el.classList.add('is-hidden');
-		} else {
-			requestAnimationFrame(fade);
-		}
-	})();
+for (var i = 0; i < btn.length; i++) {
+    btn[i].addEventListener('click', function() {
+        // console.clear();
+        // console.log("You clicked", this.innerHTML);
+            if (el.classList.contains('is-hidden')) {
+                fadeIn(el);
+                console.log("trigger fade in");
+            } else {
+                fadeOut(el);
+                console.log("trigger fade out");
+            }
+    });
 }
 
-function fadeIn(el, display) {
-	if (el.classList.contains('is-hidden')) {
-		el.classList.remove('is-hidden');
-	}
-	el.style.opacity = 0;
-	el.style.display = display || "block";
-	
-	(function fade() {
-		var val = parseFloat(el.style.opacity);
-		if (!((val += .1) > 1)) {
-			el.style.opacity = val;
-			requestAnimationFrame(fade);
-		}
-	})();
+const el = document.querySelector('.js-fade');
+
+
+const fade = () => {
+    if ((el.style.opacity -= .1) < 0) {
+        el.style.display = 'none';
+        el.classList.add('is-hidden');
+    } else {
+        requestAnimationFrame(fade);
+    }
+};
+
+// Fade animations
+const fadeOut = (el) => {
+    el.style.opacity = 1;
+    fade();
+}
+
+const fadeIn = (el, display) => {
+    if (el.classList.contains('is-hidden')) {
+        el.classList.remove('is-hidden');
+    }
+    el.style.opacity = 0;
+    el.style.display = display || "block";
+    
+    (function fade() {
+        var val = parseFloat(el.style.opacity);
+        if (!((val += .1) > 1)) {
+            el.style.opacity = val;
+            requestAnimationFrame(fade);
+        }
+    })();
 };
